2fbd0a14751061ac035b1d71f345ed3bd5831057
Teo Chistol <teodor.chistol@softwaremind.com>
2025-03-15 09:44:57 +0200

    Hide image path when mouse not moved

--- src/components/ImageSlider/ImagePathContainer.tsx
+++ src/components/ImageSlider/ImagePathContainer.tsx
@@ -1,3 +1,28 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
 export const ImagePathContainer = ({ filename }: { filename: string }) => {
-  return <div id="filename-container">{filename}</div>;
+  const [isVisible, setIsVisible] = useState(true);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  const handleCursorMove = useCallback(() => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => setIsVisible(false), 6000);
+    setIsVisible(true);
+  }, [setIsVisible]);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleCursorMove);
+    return () => {
+      document.removeEventListener("mousemove", handleCursorMove);
+    };
+  }, []);
+
+  return (
+    <div
+      id="filename-container"
+      style={!isVisible ? { opacity: 0 } : undefined}
+    >
+      {filename}
+    </div>
+  );
 };
